763e53cb42fd8009638fe2177eaf5b9f796f3c2b
Benjamin MacMillan <bgfmac@cs.washington.edu>
2019-12-04 03:51:57 -0800

    first model trained

--- src/train.py
+++ src/train.py
@@ -12,6 +12,7 @@ import copy
 from tqdm import trange
 import pdb
 
+plt.ion()
 device = 'cpu'
 
 ################        DATA        ################
@@ -129,5 +130,64 @@ def train(model, criterion, optimizer, scheduler, num_epochs=25):
     return model
 
 
-model_ft = train(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
+
+
+
+
+
+################        VISUALIZATION        ################
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+def visualize_model(model, num_images=6):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['test']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
+
+################################################################
+
+
+
+
+
+
+
+# model_ft = train(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=10)
+# torch.save(model_ft.state_dict(), './model.pt')
+
+model_ft.load_state_dict(torch.load('./model.pt'))
+model_ft.eval()
+visualize_model(model_ft)
+plt.ioff()
+plt.show()
